83e38be3b7e30b706e8532793a11e1cea7ff6cc8
Dereck Souza <dereckdsouza@mail.com>
2022-03-15 17:02:19 -0300

    versao inicial

---
+++ if_else.py
@@ -0,0 +1,19 @@
+
+#if_else.py
+
+
+temp = int(input('Entre com a temperatura:'))
+
+if temp < 0:
+    print("Congelando..")
+elif 0 <= temp <= 20:
+    print('Frio')
+
+elif 21 <= temo <= 25:
+    print('Normal')
+
+elif 26 <= temp <= 35:
+    print('Quente')
+
+else:
+    print('Muito Quente!')
